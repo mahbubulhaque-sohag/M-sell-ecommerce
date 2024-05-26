4471813ba966d1ed699c917bac69c657754144d2
sohag <mahbubulsohag810@gmail.com>
2024-05-26 10:37:38 +0600

    navbar and footer

---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,13 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <img 
+            className="w-[100%] h-[550px]"
+            src="https://plus.unsplash.com/premium_photo-1664201890375-f8fa405cdb7d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+            </img>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../components/Home/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            <h3 className="text-xl font-bold">This is Home</h3>
+        </div>
+    );
+};
+
+export default Home;
